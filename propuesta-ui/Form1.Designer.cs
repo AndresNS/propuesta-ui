3c4727ef42a244b9ead1baa922b2799679b7e2d5
Andrés Navarro S <31898794+AndresNS@users.noreply.github.com>
2021-10-12 18:51:46 -0300

    Agrega propuesta de ui

---
+++ propuesta-ui/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace propuesta_ui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.rbNuevoProducto = new System.Windows.Forms.RadioButton();
+            this.rbBuscarProducto = new System.Windows.Forms.RadioButton();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtCampo1 = new System.Windows.Forms.TextBox();
+            this.txtCampo2 = new System.Windows.Forms.TextBox();
+            this.txtCampo3 = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Controls.Add(this.txtBuscar);
+            this.groupBox1.Controls.Add(this.rbNuevoProducto);
+            this.groupBox1.Controls.Add(this.rbBuscarProducto);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 81);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Producto";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(185, 40);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(6, 42);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(173, 20);
+            this.txtBuscar.TabIndex = 2;
+            // 
+            // rbNuevoProducto
+            // 
+            this.rbNuevoProducto.AutoSize = true;
+            this.rbNuevoProducto.Location = new System.Drawing.Point(116, 19);
+            this.rbNuevoProducto.Name = "rbNuevoProducto";
+            this.rbNuevoProducto.Size = new System.Drawing.Size(144, 17);
+            this.rbNuevoProducto.TabIndex = 1;
+            this.rbNuevoProducto.TabStop = true;
+            this.rbNuevoProducto.Text = "Ingresar Nuevo Producto";
+            this.rbNuevoProducto.UseVisualStyleBackColor = true;
+            // 
+            // rbBuscarProducto
+            // 
+            this.rbBuscarProducto.AutoSize = true;
+            this.rbBuscarProducto.Location = new System.Drawing.Point(6, 19);
+            this.rbBuscarProducto.Name = "rbBuscarProducto";
+            this.rbBuscarProducto.Size = new System.Drawing.Size(104, 17);
+            this.rbBuscarProducto.TabIndex = 0;
+            this.rbBuscarProducto.TabStop = true;
+            this.rbBuscarProducto.Text = "Buscar Producto";
+            this.rbBuscarProducto.UseVisualStyleBackColor = true;
+            this.rbBuscarProducto.CheckedChanged += new System.EventHandler(this.rbBuscarProducto_CheckedChanged);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(77, 99);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(195, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtCampo1
+            // 
+            this.txtCampo1.Location = new System.Drawing.Point(77, 125);
+            this.txtCampo1.Name = "txtCampo1";
+            this.txtCampo1.Size = new System.Drawing.Size(195, 20);
+            this.txtCampo1.TabIndex = 2;
+            // 
+            // txtCampo2
+            // 
+            this.txtCampo2.Location = new System.Drawing.Point(77, 151);
+            this.txtCampo2.Name = "txtCampo2";
+            this.txtCampo2.Size = new System.Drawing.Size(195, 20);
+            this.txtCampo2.TabIndex = 3;
+            // 
+            // txtCampo3
+            // 
+            this.txtCampo3.Location = new System.Drawing.Point(77, 177);
+            this.txtCampo3.Name = "txtCampo3";
+            this.txtCampo3.Size = new System.Drawing.Size(195, 20);
+            this.txtCampo3.TabIndex = 4;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(53, 102);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 5;
+            this.lblID.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Campo 3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Campo 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Campo 1";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.ForeColor = System.Drawing.Color.White;
+            this.btnGuardar.Location = new System.Drawing.Point(139, 213);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(133, 23);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar Cambios";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Firebrick;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(12, 213);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(98, 23);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 252);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtCampo3);
+            this.Controls.Add(this.txtCampo2);
+            this.Controls.Add(this.txtCampo1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.RadioButton rbNuevoProducto;
+        private System.Windows.Forms.RadioButton rbBuscarProducto;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtCampo1;
+        private System.Windows.Forms.TextBox txtCampo2;
+        private System.Windows.Forms.TextBox txtCampo3;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ propuesta-ui/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace propuesta_ui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.rbBuscarProducto.Checked = true;
+
+            deshabilitarCampos();
+            this.btnEliminar.Enabled = false;
+            this.btnGuardar.Enabled = false;
+        }
+        private void rbBuscarProducto_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbBuscarProducto.Checked)
+            {
+                deshabilitarCampos();
+                limpiarCampos();
+                this.btnEliminar.Enabled = false;
+                this.btnGuardar.Enabled = false;
+                this.txtBuscar.Text = String.Empty;
+                this.txtBuscar.Enabled = true;
+                this.btnBuscar.Enabled = true;
+            }
+            else
+            {
+                limpiarCampos();
+                habilitarCampos();
+                this.txtID.Enabled = false;
+                this.btnEliminar.Enabled = false;
+                this.btnGuardar.Enabled = true;
+                this.btnGuardar.Text = "Crear Nuevo Producto";
+                this.txtBuscar.Text = String.Empty;
+                this.txtBuscar.Enabled = false;
+                this.btnBuscar.Enabled = false;
+
+            }
+        }
+
+        private void limpiarCampos()
+        {
+            this.txtID.Text = String.Empty;
+            this.txtCampo1.Text = String.Empty;
+            this.txtCampo2.Text = String.Empty;
+            this.txtCampo3.Text = String.Empty;
+        }
+
+        private void deshabilitarCampos()
+        {
+            this.txtID.Enabled = false;
+            this.txtCampo1.Enabled = false;
+            this.txtCampo2.Enabled = false;
+            this.txtCampo3.Enabled = false;
+        }
+
+        private void habilitarCampos()
+        {
+            this.txtID.Enabled = true;
+            this.txtCampo1.Enabled = true;
+            this.txtCampo2.Enabled = true;
+            this.txtCampo3.Enabled = true;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            habilitarCampos();
+            this.txtID.Enabled = false;
+            this.btnGuardar.Text = "Guardar Cambios";
+            this.btnGuardar.Enabled = true;
+            this.btnEliminar.Enabled = true;
+
+            // Ir a buscar producto a la base de datos
+            this.txtID.Text = this.txtBuscar.Text;
+            this.txtCampo1.Text = "Datos del producto";
+            this.txtCampo2.Text = "Más datos del producto";
+            this.txtCampo3.Text = "Otros datos del producto";
+
+            this.txtBuscar.Text = String.Empty;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            deshabilitarCampos();
+            limpiarCampos();
+
+            if (this.btnGuardar.Text == "Guardar Cambios")
+            {
+                // Actualizacion de producto
+
+                // Guardar nuevos datos en la base de datos
+
+                this.btnEliminar.Enabled = false;
+                this.btnGuardar.Enabled = false;
+                MessageBox.Show("Producto Actualizado");
+            }
+            else
+            {
+                // Creacion de producto nuevo
+
+                // Crear producto en la base de datos
+
+                this.rbBuscarProducto.Checked = true;
+                MessageBox.Show("Producto Creado");
+            }
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            // Aqui probablemente sea bueno mostrar un dialogo para 
+            // confirmar si es que esta seguro de eliminar
+
+            // Eliminar producto de la base de datos
+
+            deshabilitarCampos();
+            limpiarCampos();
+            this.btnEliminar.Enabled = false;
+            this.btnGuardar.Enabled = false;
+            MessageBox.Show("Producto Eliminado");
+        }
+    }
+}
